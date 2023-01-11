7ea0ad03e437312c3863fd0733d89863a16238a2
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-11 18:40:33 +0530

    Added : Random Value

--- src/com/bridgelabz/snakeandladder/UC2RandomValue.java
+++ src/com/bridgelabz/snakeandladder/UC2RandomValue.java
@@ -1,2 +1,10 @@
-package com.bridgelabz.snakeandladder;public class UC2RandomValue {
+package com.bridgelabz.snakeandladder;
+
+import java.util.Random;
+public class UC2RandomValue {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6)+1;
+        System.out.println("Dice Value is " +diceValue);
+    }
 }
