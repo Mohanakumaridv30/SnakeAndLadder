663dc907cb693fcb54f133c0bf11cc36ea1d9249
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-11 18:41:45 +0530

    Added : Winning Position

--- src/com/bridgelabz/snakeandladder/UC4WinningPosition.java
+++ src/com/bridgelabz/snakeandladder/UC4WinningPosition.java
@@ -1,2 +1,32 @@
-package com.bridgelabz.snakeandladder;public class UC4WinningPosition {
+package com.bridgelabz.snakeandladder;
+
+import java.util.Random;
+public class UC4WinningPosition {
+    public static void main(String[] args) {
+        int position = 0;
+        final int WINNINGPOSITION = 100;
+        while (position <= WINNINGPOSITION) {
+            Random rand = new Random();
+            int diceValue = rand.nextInt(6) + 1;
+            System.out.println("Dice Value is  " + diceValue);
+            int option = rand.nextInt(3);
+
+            if (option == 0) {
+                System.out.println("No Play");
+            } else if (option == 1) {
+                System.out.println("Ladder");
+                position += diceValue;
+                System.out.println("Current Position is " + position);
+            } else if (option == 2) {
+                System.out.println("Snake");
+                position -= diceValue;
+                System.out.println("Current Position is " + position);
+
+                if (position < 0) {
+                    position = 0;
+                    System.out.println("Current Position is ==>" + position);
+                }
+            }
+        }
+    }
 }
