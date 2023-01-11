2aa56ec7342efae3a9f4226fbd56ffd2e86c19e8
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-11 18:39:23 +0530

    Added : Starting Position

--- src/com/bridgelabz/snakeandladder/UC1StartPosition.java
+++ src/com/bridgelabz/snakeandladder/UC1StartPosition.java
@@ -1,2 +1,9 @@
-package com.bridgelabz.snakeandladder;public class UC1StartPosition {
+package com.bridgelabz.snakeandladder;
+
+public class UC1StartPosition {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at the start position "+ pos);
+    }
 }
