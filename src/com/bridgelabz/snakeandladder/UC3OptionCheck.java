06ba984366f2d7e997d7ecdb611a0f5a35d14aec
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-11 18:41:10 +0530

    Added : Option Check

--- src/com/bridgelabz/snakeandladder/UC3OptionCheck.java
+++ src/com/bridgelabz/snakeandladder/UC3OptionCheck.java
@@ -1,2 +1,23 @@
-package com.bridgelabz.snakeandladder;public class UC3OptionCheck {
+package com.bridgelabz.snakeandladder;
+
+import java.util.Random;
+public class UC3OptionCheck {
+    public static void main(String[] args) {
+        int position = 0;
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6) + 1;
+        System.out.println("Dice Value is " + diceValue);
+        int option = rand.nextInt(3);
+        if (option == 0) {
+            System.out.println("No Play");
+        } else if (option == 1) {
+            System.out.println("Ladder");
+            position += diceValue;
+            System.out.println("Current Position is " + position);
+        } else {
+            System.out.println("Snake");
+            position -= diceValue;
+            System.out.println("Current Position is " + position);
+        }
+    }
 }
